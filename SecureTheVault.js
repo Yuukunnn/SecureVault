dc59f634015685398f2da2c6db5cbff850a79617
Yukun Zhang <yukun@macbook-pro.mynetworksettings.com>
2024-03-13 17:52:01 -0400

    secure vault first draft

---
+++ SecureTheVault.js
@@ -0,0 +1,8 @@
+// the following code will pop up the lock for user
+const vaultMsg = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+// variavle declaration for the three numbers
+const num1 = 100/10;
+const num2 = 20*2;
+const num3 = 40-1;
+const msgToDisplay = vaultMsg + " " + num1 + " - " + num2 + " - " + num3;
+alert(msgToDisplay);
